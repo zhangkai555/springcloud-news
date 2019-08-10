63872e93e56b2e15a30bf30fda58f05b6aecb202
zhangkai <zhangkai@koolearn-inc.com>
2019-08-10 23:28:09 +0800

    添加注释

--- news-business-service/news-orders-ervice/src/main/java/com/news/order/feign/UserServiceFeign.java
+++ news-business-service/news-orders-ervice/src/main/java/com/news/order/feign/UserServiceFeign.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 /**
- * TODO
+ * RequestMapping必须跟cloud服务的Controller对应
+ * 不支持GET方法传递POJO,可以拆成一个个单独的属性放在参数里
  *
  * @author zhangkai
  * @date 2019年07月20日 22:22
